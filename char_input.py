e4975c4b5466e2ec2277866eb321a97e473faee4
Klemen Znidar <klemenznidar208@gmail.com>
2016-06-21 11:42:02 +0200

    Finished exercise 1

--- char_input.py
+++ char_input.py
@@ -5,6 +5,5 @@ age = raw_input("Enter your age: ")
 ages = date.today().year - int(age) + 100
 
 print "Your name is " + name + '\n'
-print "Your age is " + age
-
+print "Your age is " + age + '\n'
 print "You will be 100 years old in " + str(ages)
